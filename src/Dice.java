218c50b3b271b361c64e0c8a045a951987987495
Kosuke Hanashiro <e175757@ie.u-ryukyu.ac.jp>
2017-11-28 11:56:41 +0900

    commit1

---
+++ src/Dice.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.ex3_2.pair25;
+
+public class Dice{
+    private int value;
+
+    public Dice(){
+	play();
+    }
+    public int getValue(){
+	return value;
+    }
+    public void setValue(int value){
+	this.value=value;
+    }
+    public void play(){
+	value=(int)(Math.random()*6)+1;
+    }
+}
